d74a3e13d4ebd8147b7c016d24e35ca1f342fa81
Himalayan-Coder <125664476+Atuloli01@users.noreply.github.com>
2025-05-11 00:54:08 +0530

    Update purchaseApi.js

--- client/src/features/api/purchaseApi.js
+++ client/src/features/api/purchaseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "https://lms-backend-huvp.onrender.com/api/v1/purchase";
+const COURSE_PURCHASE_API = "https://lms-backend-w02y.onrender.com/api/v1/purchase";
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",

d0d02a484124ef2436fd6fac9e6b32307111cf9d
Himalayan-Coder <125664476+Atuloli01@users.noreply.github.com>
2025-05-11 00:25:47 +0530

    Update courseProgressApi.js

--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "http://localhost:8088/api/v1/progress";
+const COURSE_PROGRESS_API = "https://lms-backend-huvp.onrender.com/api/v1/progress";
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
